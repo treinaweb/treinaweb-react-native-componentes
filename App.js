59b1d316d223ca4484f1e38b38352b9070457332
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-04-15 16:42:48 -0300

    aula-04-03

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Image, ImageBackground, Text } from 'react-native';
+import {StyleSheet, View, FlatList, Text } from 'react-native';
 
+const myList = [];
+for(let i = 0; i < 40; i++){
+  myList.push({id: i.toString()});
+}
 
 export default class App extends Component {
   componentDidMount(){
-    
+    setTimeout(() => {
+      //this.item.scrollToIndex({index: 20})
+      //this.item.scrollToItem({item: {}})
+      //this.item.scrollToEnd()
+    }, 2000)
+  }
+
+  keyExtractor(item){
+    return item.id;
   }
 
   render() {
     return (
       <View style={styles.container} >
-        <Image style={{width: 150, height: 100}}
-        source={{uri: 'https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700'}} />
-        
-        <ImageBackground style={{width: 150, height: 100}} source={{uri: 'https://img1.ibxk.com.br/2017/07/13/13160112901226.jpg?w=700'}} >
-          <Text style={{color: 'white'}} >Treinaweb</Text>
-        </ImageBackground>
+        <FlatList 
+          data={myList}
+          ref={(item) => this.item = item}
+          keyExtractor={this.keyExtractor}
+          renderItem={({item}) => <Text>{item.id}</Text>}
+
+          ListEmptyComponent={() => <Text>Nenhum Item</Text>}
+          
+          ListHeaderComponent={() => <Text>TOP</Text>}
+          ItemSeparatorComponent={() => <Text>SEP</Text>}
+          ListFooterComponent={() => <Text>BOTTOM</Text>}
+        />
       </View>
     );
   }
