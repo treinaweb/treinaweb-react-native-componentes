f62407a6c08e77a0775983cbfbe8caf18e306bf1
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-04-15 16:06:48 -0300

    aula-04-01

--- App.js
+++ App.js
@@ -1,45 +1,16 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, ToolbarAndroid } from 'react-native';
+import {StyleSheet, View, Text } from 'react-native';
 
-const icon = {uri: 'http://icons.iconarchive.com/icons/paomedia/small-n-flat/256/sign-question-icon.png'};
 
 export default class App extends Component {
   componentDidMount(){
     
   }
 
-  handleAction(position){
-    console.log(position);
-  }
-
   render() {
     return (
-      <View>
-        <ToolbarAndroid 
-          title="Meu App"
-          subtitle="Desc. App"
-          navIcon={icon}
-          titleColor="white"
-          subtitleColor="red"
-          actions={[
-            {
-              title: 'Config.',
-              show: 'always',
-              icon: icon
-            },
-            {
-              title: 'Config.',
-              show: 'always',
-              icon: icon
-            }
-          ]}
-          style={{
-            backgroundColor: '#8196F3',
-            height: 56
-          }}
-          onActionSelected={this.handleAction}
-          onIconClicked={this.handleAction}
-         />
+      <View style={styles.container} >
+        <Text ellipsizeMode={"tail"} numberOfLines={2} >Treinaweb</Text>
       </View>
     );
   }
