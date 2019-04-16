2cf36644e5d576a1730bfbd34f11e994cea8f6ca
hanashiro <hanashiro@Felipes-MacBook-Pro.local>
2019-04-16 08:09:35 -0300

    aula-05-01

--- App.js
+++ App.js
@@ -1,52 +1,30 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, FlatList, Text, RefreshControl } from 'react-native';
+import {StyleSheet, View, TextInput } from 'react-native';
 
-const myList = [];
-for(let i = 0; i < 20; i++){
-  myList.push({id: i.toString()});
-}
 
 export default class App extends Component {
+
   state = {
-    isLoading: false,
-    myList
+    nome: ''
   }
 
   componentDidMount(){
     
   }
 
-  keyExtractor(item){
-    return item.id;
-  }
-
-  update = () => {
-    this.setState({isLoading: true});
-    setTimeout(() => {
-      this.setState({
-        isLoading: false,
-        myList: [
-          {id: '123'},
-          {id: '456'},
-          {id: '789'}
-        ]
-      })
-    }, 5000)
-  }
-
   render() {
     const {state} = this;
     return (
-      <View style={styles.container} >
-        <FlatList 
-          data={state.myList}
-          ref={(item) => this.item = item}
-          keyExtractor={this.keyExtractor}
-          renderItem={({item}) => <Text>{item.id}</Text>}
-          refreshControl={
-            <RefreshControl refreshing={state.isLoading} onRefresh={this.update} />
-          }
-        />
+      <View >
+        <TextInput 
+          keyboardType="default"
+          maxLength={20}
+          editable={true}
+          multiline={true}
+          numberOfLines={4}
+          style={{borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => { this.setState({nome: text}) }}
+          value={state.nome}  />
       </View>
     );
   }
